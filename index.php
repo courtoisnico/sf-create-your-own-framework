a6e708f4dc91c61b3c92b21c23491b35d29bc94b
ncourtois <ncourtois@riu.fr>
2018-03-26 11:51:26 +0200

    End page 2

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
  * Date: 21/03/18
  * Time: 15:00
  */
-$name = isset($_GET['name']) ? $_GET['name'] : 'World';
 
-header('Content-Type: text/html; charset=utf-8');
+require_once __DIR__.'/vendor/autoload.php';
 
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+//$name = isset($_GET['name']) ? $_GET['name'] : 'World';
+$name = $request->get('name', 'World');
+
+//header('Content-Type: text/html; charset=utf-8');
+//printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
