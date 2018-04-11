32be8d0bad0be54665764f620be0ba78a4d17030
ncourtois <ncourtois@riu.fr>
2018-04-11 15:21:02 +0200

    FDJ

--- web/front.php
+++ web/front.php
@@ -10,44 +10,34 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
-//$response = new Response();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-/*$map = array(
-    '/hello' => __DIR__.'/../src/pages/hello.php',
-    '/bye' => __DIR__.'/../src/pages/bye.php',
-);
-
-$path = $request->getPathInfo();*/
+/*$routes->add('hello', new Routing\Route('hello/{name}', array(
+    'name' => 'World',
+    '_controller' => 'render_template',
+)));*/
 
 try {
-    extract($request->query->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
     $response = new Response('An error occured', 500);
 }
-
-/*if (isset($map[$path])) {
-    ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-//    include $map[$path];
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
-    //$response->setContent(ob_get_clean());
-    $response = new Response(ob_get_clean());
-} else {
-    //$response->setStatusCode(404);
-    //$response->setContent('Not Found');
-    $response = new Response('Not Found', 404);
-}*/
 
 $response->send();
